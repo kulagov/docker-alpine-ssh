e4e7f480c912b2568ee68fa316756a2dad8c0a62
Anton <46803534+devdotnetdotorg@users.noreply.github.com>
2020-11-26 00:17:13 +0300

    Update buildx-tags.sh

--- buildx-tags.sh
+++ buildx-tags.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# Install buildx
+# $ export DOCKER_BUILDKIT=1
+# $ docker build --platform=local -o . git://github.com/docker/buildx
+# $ mkdir -p ~/.docker/cli-plugins
+# $ mv buildx ~/.docker/cli-plugins/docker-buildx
+
+# Execute each time before building with buildx
+# $ export DOCKER_BUILDKIT=1
+# $ docker run --rm --privileged docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad64
+# $ cat /proc/sys/fs/binfmt_misc/qemu-aarch64
+
 set -e
 
 echo "Start BUILDX"
